9e365d70f3ac254cc48317302bdc31cbb39f117e
Helmi Estiawan <helmiestiawan@gmail.com>
2025-10-21 23:57:15 +0700

    update model enum

--- iniprojecthelmi/products/models.py
+++ iniprojecthelmi/products/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from enum import Enum
+
+class CategoryProduct(Enum):
+    PARALLETTES = 'Paralleltes'
+    ACCESSORIES = 'Accessories'
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
+    category = models.CharField(
+        max_length=100,
+        choices=[(status.value, status.name) for status in CategoryProduct],
+        default=CategoryProduct.PARALLETTES.value,
+    )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     description = models.TextField(blank=True, null=True)
