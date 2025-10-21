130d2127c0d5322c4800489c6a0aacb338fba38c
Helmi Estiawan <helmiestiawan@gmail.com>
2025-10-22 00:18:53 +0700

    update form with enum choices

--- iniprojecthelmi/products/views.py
+++ iniprojecthelmi/products/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Product
+from .models import Product, CategoryProduct
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
 
 def product_create(request):
+    choices = [(status.value, status.name) for status in CategoryProduct]
+
     if request.method == 'POST':
         name = request.POST['name']
         category = request.POST['category']
         price = request.POST['price']
         stock = request.POST['stock']
         description = request.POST.get('description', '')
-        Product.objects.create(name=name, category=category, price=price, stock=stock, description=description)
+        Product.objects.create(
+            name=name,
+            category=category,
+            price=price,
+            stock=stock,
+            description=description
+        )
         return redirect('product_list')
-    return render(request, 'products/product_form.html')
+
+    return render(request, 'products/product_form.html', {
+        'product': None,
+        'category_choices': choices,
+    })
 
 def product_update(request, id):
     product = get_object_or_404(Product, id=id)
