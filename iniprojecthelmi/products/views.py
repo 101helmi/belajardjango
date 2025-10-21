aceb23feb6fbc63b202e888a526230d34acc83a4
Helmi Estiawan <helmiestiawan@gmail.com>
2025-10-21 22:53:22 +0700

    Initial project

---
+++ iniprojecthelmi/products/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+def product_create(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        category = request.POST['category']
+        price = request.POST['price']
+        stock = request.POST['stock']
+        description = request.POST.get('description', '')
+        Product.objects.create(name=name, category=category, price=price, stock=stock, description=description)
+        return redirect('product_list')
+    return render(request, 'products/product_form.html')
+
+def product_update(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        product.name = request.POST['name']
+        product.category = request.POST['category']
+        product.price = request.POST['price']
+        product.stock = request.POST['stock']
+        product.description = request.POST.get('description', '')
+        product.save()
+        return redirect('product_list')
+    return render(request, 'products/product_form.html', {'product': product})
+
+def product_delete(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'products/product_confirm_delete.html', {'product': product})
